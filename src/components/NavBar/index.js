c932ef8f023162afc80411dacfbd775207503292
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2022-03-16 23:46:05 -0600

    fix(style): Adjust elements spacing

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className="container">
+    <div>
       <center>
         <h1 className="text-gradient-orange fs-600">JOPIN</h1>
       </center>
--- src/screens/WelcomeScreen/index.js
+++ src/screens/WelcomeScreen/index.js
@@ -1,35 +1,23 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
 import welcomeImg from "../../assets/images/welcome-img.svg";
+import './WelcomeScreen.css';
 
 const WelcomeScreen = () => {
   return (
     <div>
       <NavBar />
-      <div className="flex">
+      <div className="flex column">
         <img
           src={welcomeImg}
           alt="Welcome"
           className="center-img"
-          style={{ marginTop: "50px" }}
         />
-        <div className="title">
-          Bienvenido
-        </div>
-        <div className="subtitle">
-          Basta de pedir enlaces, deja que lo <br></br> hagamos por ti.
-        </div>
-        <button
-          className="common-btn btn-shadow fs-500"
-          style={{
-            padding: "10.5px 73.5px 10.5px 73.5px",
-            marginLeft: "69px",
-            marginRight: "69px",
-            marginTop: "10px",
-          }}
-        >
-          COMENZAR
-        </button>
+        <p className="title">Bienvenido</p>
+        <p className="subtitle text-gray">
+          Basta de pedir enlaces, deja que lo<br></br>hagamos por ti.
+        </p>
+        <button className="primary-btn btn-shadow fs-500">COMENZAR</button>
       </div>
     </div>
   );

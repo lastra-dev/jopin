db0acd4593f0d4d75fadc7ed3e28d803165aa190
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2022-03-28 17:58:10 -0600

    chore(Tiles): Change checkbox to switch

--- src/components/SubjectTile/index.js
+++ src/components/SubjectTile/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import './SubjectTile.css'
+import Switch from "react-switch";
 
 const SubjectTile = (props) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleChecked = toggle => {
+    setChecked(toggle);
+  };
+
   return (
     <div className='flex tile'>
       <p>{props.hour}</p>
@@ -9,7 +16,18 @@ const SubjectTile = (props) => {
         <div className='subject-name'>
           {props.name}
         </div>
-        <input type='checkbox'></input>
+        < Switch
+          checked={checked}
+          onChange={handleChecked}
+          checkedIcon={false}
+          uncheckedIcon={false}
+          height={18}
+          width={40}
+          handleDiameter={22}
+          boxShadow="0px 1px 3px rgba(0, 0, 0, 0.6)"
+          activeBoxShadow="0px 0px 1px 5px rgba(0, 0, 0, 0.2)"
+          onColor="#FF5900"
+        />
       </div>
     </div>
   );

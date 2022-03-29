433e85a9883d5cabbfa8e83a23a6fc9f8eb1bee3
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2022-03-28 23:16:52 -0600

    fix(HomeScreen): Change space between elements

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -9,20 +9,21 @@ import './HomeScreen.css';
 const HomeScreen = () => {
   return (
     <div>
-      <div className='flex home-screen-navbar' >
+      <div className='flex home-screen-navbar'>
         <AddSubject /> <NavBar /> <SettingBtn />
       </div>
+      <hr></hr>
       <div className='flex weekday' >
         <ArrowLeft />
         <p className='title'>Lunes</p>
         <ArrowRight />
       </div>
       <div className='flex column tiles'>
-        <SubjectTile hour='10:00 am' name='Materia de Rellenoooooooooooooooo' />
-        <SubjectTile hour='10:00 am' name='Materia de Rellenoooooooooooooooo' />
-        <SubjectTile hour='10:00 am' name='Materia de Rellenoooooooooooooooo' />
-        <SubjectTile hour='10:00 am' name='Materia de Rellenoooooooooooooooo' />
-        <SubjectTile hour='10:00 am' name='Materia de Rellenoooooooooooooooo' />
+        <SubjectTile hour='10:00 am' name='Sistemas Operativos I' />
+        <SubjectTile hour='1:00 pm' name='Tratamiento estadístico de la información' />
+        <SubjectTile hour='4:00 pm' name='Desarrollo de aplicaciones web II' />
+        <SubjectTile hour='5:30 pm' name='Desarrollo de Software' />
+        <SubjectTile hour='7:00 pm' name='Redes Avanzadas' />
       </div>
     </div>
   );

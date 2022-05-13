a6c023b623ac0e0bd85f3e1b88eb10f3ca33f568
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2022-05-13 17:26:51 -0500

    fix(Schedule): Avoid url opening on past schedule

--- public/background.js
+++ public/background.js
@@ -5,5 +5,11 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
+  // Avoids opening tab when scheduled before current date
+  // -10 seconds to have a reasonable threshold of time.
+  // without -10 seconds, Date.now() > alarm.scheduledTime would
+  // always be true
+  if (Date.now() - 10000 > alarm.scheduledTime) { return; }
+
   chrome.tabs.create({ url: alarm.name.split(" ")[0] })
 })
